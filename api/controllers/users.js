3402f7a00d684b6a0aee20c1435597f945361588
kelzk <liasdfg606@gmail.com>
2023-04-11 21:56:22 +0800

    Update controller users.js

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -6,10 +6,11 @@ export const updatePassword = (req, res) => {
     
     const q2 = 'SELECT password FROM users WHERE uid = ?';
     db.query(q2, [req.body.uid], (err, data) => {
-        if (data[0].password != req.body.currentPassword) return res.status(400).json("Current password not match!");
+        if (err) return res.status(500).json(err);
+        if (data[0].password != req.body.currentPassword) return res.status(409).json("Current password not match!");
     });
 
-    if (req.body.newPassword != req.body.newPasswordConfirmation) return res.status(400).json("New password not match!");
+    if (req.body.newPassword != req.body.newPasswordConfirmation) return res.status(409).json("New password not match!");
 
     // SQL query
     const q = 'UPDATE users SET password = ? WHERE uid = ?';
